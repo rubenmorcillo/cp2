cf08ba43eea3d1a5cfdd7e2de225b8022e22898a
rubenmorcillo <ravent953@gmail.com>
2019-06-25 21:49:43 +0200

    Actualizados Fixtures con nuevas entidades +Añadido el bundle nelmio/alice a appKernel

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -17,6 +17,7 @@ class AppKernel extends Kernel
             new Doctrine\Bundle\DoctrineBundle\DoctrineBundle(),
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
             new Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle(),
+            new Nelmio\Alice\Bridge\Symfony\NelmioAliceBundle(),
             new AppBundle\AppBundle(),
         ];
 
